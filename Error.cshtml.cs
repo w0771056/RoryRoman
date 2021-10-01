5ba2a54c29df69609c6bbce8ce5cde9411498c0f
w0771056 <90808231+w0771056@users.noreply.github.com>
2021-10-01 03:59:44 -0400

    Add files via upload

--- Error.cshtml.cs
+++ Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Rory.Pages
+namespace RoryRomanovich.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
